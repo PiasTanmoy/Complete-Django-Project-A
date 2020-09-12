fc294c4c6fc5ff222f220a466c328dde2beb524d
PiasTanmoy <pias1600@gmail.com>
2020-09-12 14:00:01 +0600

    Initial commit

---
+++ StudentManagement/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200, unique=True)
+    contact_no = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
---
+++ StudentManagement/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import Student
+from .forms import StudentForm
+
+# Create your views here.
+
+
+def showStudents(request):
+    studentList = Student.objects.all()
+    context = {
+        'students' : studentList
+    }
+    return render(request, 'StudentManagement/studentlist.html', context)
+
+
+def insertStudent(request):
+
+    message = ""
+    form = StudentForm()
+
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        message = "Invalid input. Please try again!"
+        if form.is_valid():
+            form.save()
+            message = "Student is inserted to DB. You can insert a new student now"
+            form = StudentForm()
+
+    context = {
+        'form' : form,
+        'message' : message
+    }
+    return render(request, 'StudentManagement/insertStudent.html', context)
